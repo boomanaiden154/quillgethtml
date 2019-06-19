16ab391e34a10b9d917389c39e5071b45fecb1b4
Aiden Grossman <agrossman154@yahoo.com>
2019-06-18 18:52:59 -0700

    Fixed the functionality regarding new lines

--- quillgethtml.js
+++ quillgethtml.js
@@ -1,45 +1,70 @@
 Quill.prototype.getHTML = function() {
     var delta = this.getContents();
     var output = "";
+    var lines = [];
+    lines.push("");
+    linesIndex = 0;
     delta.ops.forEach(function(element) {
         if(element.attributes)
         {
             if(element.attributes.bold)
             {
-                output += "<b>";
+                lines[linesIndex] += "<b>";
             }
             if(element.attributes.italic)
             {
-                output += "<i>";
+                lines[linesIndex] += "<i>";
             }
             if(element.attributes.underline)
             {
-                output += "<u>";
+                lines[linesIndex] += "<u>";
+            }
+            if(element.attributes.strike)
+            {
+                lines[linesIndex] += "<strike>";
             }
         }
         if(element.insert.formula)
         {
-            output += "<span>\\(" + element.insert.formula + "\\)</span>";
+            lines[linesIndex] += "<span>\\(" + element.insert.formula + "\\)</span>";
         }
         else
         {
-            output += element.insert;
+            element.insert.split(/(\n)/g).forEach(function(textElement) {
+                if(textElement == "\n")
+                {
+                    lines[linesIndex] = lines[linesIndex] + "</br>"
+                    lines.push("");
+                    linesIndex++;
+                }
+                else
+                {
+                    lines[linesIndex] += textElement;
+                }
+            });
         }
         if(element.attributes)
         {
             if(element.attributes.bold)
             {
-                output += "</b>";
+                lines[linesIndex] += "</b>";
             }
             if(element.attributes.italic)
             {
-                output += "</i>"
+                lines[linesIndex] += "</i>"
             }
             if(element.attributes.underline)
             {
-                output += "</u>"
+                lines[linesIndex] += "</u>"
+            }
+            if(element.attributes.strike)
+            {
+                lines[linesIndex] += "</strike>";
             }
         }
+    });
+    lines.forEach(function(element) {
+        output += element;
     });
     return output;
 };
